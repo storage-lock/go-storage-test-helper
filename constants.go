492353c844c5d3beb3a84ea87b0d70a52eb5ee04
CC11001100 <CC11001100@qq.com>
2023-09-03 02:07:52 +0800

    doc: 为测试时用到的常量增加注释

--- constants.go
+++ constants.go
@@ -4,13 +4,22 @@ import "time"
 
 // 单元测试统一使用的一些常量
 const (
-	TestDatabaseName = "storage_lock_test"
-	TestTableName    = "storage_lock_test"
 
-	TestLockId      = "lock_id_for_test"
+	// TestDatabaseName 测试时使用到的数据库的名称
+	TestDatabaseName = "storage_lock_test"
+
+	// TestTableName 测试时使用到的表的名称
+	TestTableName = "storage_lock_test"
+
+	// TestLockId 测试时使用的锁的名称
+	TestLockId = "lock_id_for_test"
+
+	// TestLockVersion 测试时使用到的版本号
 	TestLockVersion = 1
 
+	// TestOwnerIdA 竞争锁的其中一个owner
 	TestOwnerIdA = "owner_id_A"
+	// TestOwnerIdB 竞争锁的另一个owner
 	TestOwnerIdB = "owner_id_B"
 )
 
